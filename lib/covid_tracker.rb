592a060927ee11c04cd22634f1e16d1d305623f8
stacysorey <stacysorey@gmail.com>
2020-12-05 17:00:44 -0500

    first commit

---
+++ lib/covid_tracker.rb
@@ -0,0 +1,6 @@
+require "covid_tracker/version"
+
+module CovidTracker
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/covid_tracker/version.rb
@@ -0,0 +1,3 @@
+module CovidTracker
+  VERSION = "0.1.0"
+end
