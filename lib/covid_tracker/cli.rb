1d07006383b237cdfd6fab2de0ef4606d6a50ac6
stacysorey <stacysorey@gmail.com>
2020-12-07 00:52:26 -0500

    updated cli

--- lib/covid_tracker/cli.rb
+++ lib/covid_tracker/cli.rb
@@ -1,10 +1,17 @@
 class CLI
+    @@cyn="\e[1;36m"
+    @@blu="\e[1;34m"
+    @@grn="\e[1;32m"
+    @@red="\e[1;31m"
+    @@underline="\e[1;4;31m"
+    @@rem_line="\e[1;0m"
+    @@white="\e[0m"
 
     def call
         puts ""
-        puts "------Welcome to your COVID-19 report.------"
+        puts "#{@@underline}------Welcome to Your COVID-19 Report------"
         puts ""
-        puts "-------Information is updated daily.--------"
+        puts "-------Information is Updated Daily--------#{@@rem_line}"
         puts ""
         start
     end
@@ -12,7 +19,8 @@ class CLI
     def start
         
         puts ""
-        puts "Enter (N) for national data OR enter (S) for state data"
+        puts "#{@@blu}Enter #{@@grn}(N) #{@@blu}for national data OR enter #{@@grn}(S) #{@@blu}for state data"
+        puts "Enter #{@@grn}(E) #{@@blu}to exit"
         puts ""
 
         input = gets.strip.downcase
@@ -22,61 +30,83 @@ class CLI
         elsif input == "n"
             API.national_data
             list_national_data
+        elsif input == "e"
+            goodbye
         else
-            puts "Incorrect input"
+            puts "#{@@red}***Incorrect input***"
             start
         end
+        
 
         puts ""
-        puts "Would you like to see another report? Enter (Y) or (N)"
+        puts "#{@@blu}Would you like to see another report? Enter #{@@grn}(Y) #{@@blu}or #{@@grn}(N)"
         puts ""
 
         input = gets.strip.downcase
         if input == "y"
             start
         elsif input == "n"
-            puts ""
-            puts ""
-            puts "Be safe and wear a mask!"
-            puts ""
-            exit
+           goodbye
         else
             puts ""
-            puts "Incorrect input."
+            puts "#{@@red}***Incorrect input***"
             start
         end
     end
 
+    def goodbye
+        puts ""
+        puts ""
+        puts "#{@@cyn}Be safe and wear a mask!"
+        puts ""
+        exit
+    end
+
    def pick_state
+    puts ""
+    puts "#{@@underline}----Input number for desired state----#{@@rem_line}"
+    #put a note for territories
+    puts ""
+    puts ""
     states = Stats.find_states
     states.each.with_index(1) do |state, index|
-        puts "#{index}. #{state.state}"
+        puts "#{@@red}#{index}. #{@@white}#{state.state}#{@@red}"
     #input = gets.strip.to_i
     
     end
-    puts "Pick a number of a state you want a report for"
+    
     input = gets.strip.to_i
-    list_state_data(input)
-    #make sure it doesn't go over 56
+    if input == /[1-56]/ #FIXXXXXXXXXXXXXXX
+        list_state_data(input)
+    else
+        puts "Invalid input."
+        puts "Would you like to select again? (Y) or (N)"
+        pick = gets.strip.downcase
+        if pick == "y"
+        pick_state
+        else
+            goodbye
+        end
+    end
    end
 
    def list_state_data(input)
     index = input - 1
-    state = Stats.find_states[index]
+    state = Stats.new.find_states[index]
     puts ""
-    puts "Report for #{state.state} on #{state.date}:"
+    puts "#{@@underline}Report for #{state.state} on #{state.date}:#{@@rem_line}#{@@red}"
     list_stats(state)
-    puts "Total Tested Cases:   #{state.total}"
+    puts "Total Tested Cases:   #{@@white}#{state.total}"
     puts ""
     puts ""
    end
    #if called multiple times, lists states multiple times (up to 160s)
-   #put a note about territories
+ 
 
    def list_national_data
     nat = Stats.find_national
     puts ""
-    puts "National Report for #{nat.date}:"
+    puts "#{@@underline}National Report for #{nat.date}#{@@rem_line}#{@@red}:"
     list_stats(nat)
     
    end
@@ -84,11 +114,11 @@ class CLI
    def list_stats(stat)
     puts ""
     puts ""                 
-    puts "Total Positive Cases: #{stat.positive}"
-    puts "Total Negative Cases: #{stat.negative}"
-    puts "Total Recovered:      #{stat.recovered}"
-    puts "Total Deaths:         #{stat.death}"
-    puts "Total Hospitalized:   #{stat.hospitalized}"
+    puts "Total Positive Cases: #{@@white}#{stat.positive}#{@@red}"
+    puts "Total Negative Cases: #{@@white}#{stat.negative}#{@@red}"
+    puts "Total Recovered:      #{@@white}#{stat.recovered}#{@@red}"
+    puts "Total Deaths:         #{@@white}#{stat.death}#{@@red}"
+    puts "Total Hospitalized:   #{@@white}#{stat.hospitalized}#{@@red}"
    end
 
 end
